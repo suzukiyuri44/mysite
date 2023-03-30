7a836c8812d006731256dbe65a53745fd8215a88
suzukiyuri <rintarou.rasras.0811@gmail.com>
2023-03-30 23:31:21 +0900

    lesson10 修正

--- Java_curriculum/src/Java_curriculum/src/lesson10/practice.java
+++ Java_curriculum/src/Java_curriculum/src/lesson10/practice.java
@@ -17,10 +17,11 @@ public class practice {
 
 	public static void main(String[] args) {
 		
-		for(int i = 1;i<=21;i+=2) {
-			System.out.println(i);
+		for(int i=1;i<=20;i+=2) {
+			System.out.println(i+2);
 			}
-		
+	
+	
 	}
 
 }
