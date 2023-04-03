c8fd173da62e0371f7a64db0770a1e142d162b84
suzukiyuri <rintarou.rasras.0811@gmail.com>
2023-04-03 22:26:05 +0900

    lesson10 修正　no2

--- Java_curriculum/src/Java_curriculum/src/lesson10/practice2.java
+++ Java_curriculum/src/Java_curriculum/src/lesson10/practice2.java
@@ -13,8 +13,10 @@ public class practice2 {
 
 	public static void main(String[] args) {
 		for(int i =1;i <=9;i++) {
-			for(int a = 1;a<=9;a++)
-				System.out.println(i+"×"+a+"="+i*a);
+			for(int a = 1;a<=9;a++) {
+				System.out.print(i+"×"+a+"="+i*a+"  ");
+			}
+			System.out.println();	
 		}
 	}
 }
