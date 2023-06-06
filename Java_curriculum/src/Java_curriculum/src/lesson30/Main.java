9bb6cf1a73b043f0e103be50c867e8b03530a311
suzukiyuri <rintarou.rasras.0811@gmail.com>
2023-06-06 23:57:03 +0900

    lesson30 修正①

--- Java_curriculum/src/Java_curriculum/src/lesson30/Main.java
+++ Java_curriculum/src/Java_curriculum/src/lesson30/Main.java
@@ -3,11 +3,11 @@ package lesson30;
 public class Main {
 
 	public static void main(String[] args) {
-		Person person1 = new Person("鈴木太郎",20,1.7,60);
+		Person person1 = new Person("鈴木太郎", 20, 1.7, 60);
 		System.out.println(person1.name);
 		System.out.println(person1.age);
-		System.out.println(person1.heigth);
-		
+		System.out.println(person1.height);
+
 		person1.print();
 		Person.Count();
 	}
--- Java_curriculum/src/Java_curriculum/src/lesson30/Person.java
+++ Java_curriculum/src/Java_curriculum/src/lesson30/Person.java
@@ -4,34 +4,34 @@ public class Person {
 	// インスタンスフィールドを定義
 	public String name;
 	public int age;
-	public  double heigth;
-	public double weigth;
+	public double height;
+	public double weight;
 	public static int count = 0;
-	
+
 	// コンストラクタを定義しインスタンスフィールドに値をセット
-	Person(String name,int age,double heigth,double weigth){
-		this.name =name;
+	Person(String name, int age, double height, double weight) {
+		this.name = name;
 		this.age = age;
-		this.heigth = heigth;
-		this.weigth = weigth;
+		this.height = height;
+		this.weight = weight;
 		Person.count++;
 	}
-	
-	//インスタンスメソッド「bmi」を定義
+
+	// インスタンスメソッド「bmi」を定義
 	public double bmi() {
-		return this.weigth/Math.pow(this.heigth, 2);
+		return this.weight / (this.height * this.height);
 	}
-	
-	//インスタンスメソッド「print」を定義
+
+	// インスタンスメソッド「print」を定義
 	public void print() {
 		System.out.println("名前は" + this.name + "です");
-		System.out.println("年は"+this.age+"です。");
-		System.out.println("BMIは"+String.format("%.1f",bmi())+"です。");
+		System.out.println("年は" + this.age + "です。");
+		System.out.println("BMIは" + String.format("%.1f", this.bmi()) + "です。");
 	}
-	
-	//人数の合計
-	public static void Count(){
+
+	// 人数の合計
+	public static void Count() {
 		System.out.println("合計" + Person.count + "人です。");
-		}
+	}
 
 }
