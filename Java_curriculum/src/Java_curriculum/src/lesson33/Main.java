2a9ca23769db0a256bd789782258b1cc53174b3f
suzukiyuri <rintarou.rasras.0811@gmail.com>
2023-07-07 19:00:32 +0900

    lesson33 修正①

--- Java_curriculum/src/Java_curriculum/src/lesson33/Main.java
+++ Java_curriculum/src/Java_curriculum/src/lesson33/Main.java
@@ -3,10 +3,17 @@ package lesson33;
 public class Main {
 
 	public static void main(String[] args) {
+		// Personメソッドをperson2の値入力
 		Person person1 = new Person("鈴木", "太郎", 20, 1.7, 60);
+		// person1の値をprintメソッドを入力し、コンソールに出力
 		person1.print();
+		
+		// Personメソッドをperson2の値入力
 		Person person2 = new Person("山田", "花子", 22, 1.5, 40);
+		// person2の値をprintメソッドを入力し、コンソールに出力
 		person2.print();
+		
+		// printCountの値を出力
 		Person.printCount();
 	}
 
